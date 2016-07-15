b85c2924b3740955f6f54fb68bb2e0c1047aab42
张元科 <zhangyuanke@leador.com.cn>
2016-07-15 22:55:40 +0800

    the first commit 2

---
+++ iStudySwift/iStudySwift/Classes/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  iStudySwift
+//
+//  Created by zhangyuanke on 16/7/15.
+//  Copyright © 2016年 zhangyuanke. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+
+}
